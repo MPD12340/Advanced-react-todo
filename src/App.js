e39a756704b5484c6b491c954d002c554bc4939b
Mahendra Dhamala <rijalmpd1234@gmail.com>
2023-01-08 23:20:26 +0545

    initial commit

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from 'react'
+import List from './List'
+import Alert from './Alert'
+
+
+const getLocalStorage = ()=>{
+  let list = localStorage.getItem('list')
+  if(list){
+    return JSON.parse(localStorage.getItem('list'))
+  }
+  else{
+    return []
+  }
+}
+
+function App() {
+  const[name , setName] = useState("");
+  const [list ,setList] = useState(getLocalStorage());
+  const [isEditing , setIsEditing] = useState(false)
+  const [editId , setEditId] = useState(null);
+  const [alert , setAlert] = useState({show:false , msg:'' , type:''})
+
+const handleSubmit=(e)=>{
+e.preventDefault();
+if(!name){
+  //alert
+ 
+    showAlert(true, "please fill the form", "danger");
+ 
+  
+}
+else if(name && isEditing){
+  //deal with edit
+  setList(list.map((item)=>{
+
+    if(item.id === editId){
+      return {...item , title:name }
+    }
+    return item;
+}))
+setName("")
+setEditId(null)
+setIsEditing(false)
+showAlert(true , "value changed" , "success")
+}
+else{
+  //alert
+  showAlert(true , "item added to the list" , "success")
+  const newItem = {id: new Date().getTime().toString(), title : name};
+  setList([...list , newItem]);
+  setName('')
+}
+}
+
+const deleteitem=(id)=>{
+const newlist = list.filter((item)=>item.id !== id);
+setList(newlist)
+}
+
+const editItem = (id)=>{
+  const specificItem = list.find((item)=> item.id === id);
+  setIsEditing(true)
+  setEditId(id)
+  setName(specificItem.title)
+}
+
+const showAlert = (show ="false" , msg ="", type="")=>{
+setAlert({show ,msg , type })
+}
+
+const clearList =()=>{
+  showAlert(true , "empty list" , "danger")
+  setList([])
+}
+
+useEffect(()=>{
+localStorage.setItem('list' , JSON.stringify(list))
+},[list])
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert = {showAlert} list={list}/>}
+        <h3>Add Your Task</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value ={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "Edit" : "Submit"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} deleteitem={deleteitem} editItem = {editItem}/>
+          <button className="clear-btn" onClick={clearList}>clear items</button>
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default App
